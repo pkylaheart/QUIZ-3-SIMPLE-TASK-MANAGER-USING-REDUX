129bc40a0a76379f1f29520aabdca1c28b8bfcd5
Kyla Heart Pineda <125124933+pkylaheart@users.noreply.github.com>
2026-05-12 19:00:43 +0800

    update

--- simple-task-manager/src/App.js
+++ simple-task-manager/src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addTask,
+  deleteTask,
+} from "./feature/taskSlice";
 
-function App() {
+import { useState } from "react";
+
+import "./index.css";
+
+export default function App() {
+  const [taskNameInput, setTaskNameInput] =
+    useState("");
+
+  const tasks = useSelector(
+    (state) => state.tasks
+  );
+
+  const dispatch = useDispatch();
+
+  const handleAddTask = (e) => {
+    e.preventDefault();
+
+    if (!taskNameInput.trim()) {
+      alert("Please enter a task!");
+      return;
+    }
+
+    dispatch(
+      addTask({
+        id: Date.now(),
+        name: taskNameInput,
+      })
+    );
+
+    setTaskNameInput("");
+  };
+
+  const handleClearList = () => {
+    tasks.forEach((task) => {
+      dispatch(deleteTask(task.id));
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="main-container">
+
+      <div className="task-card">
+
+        <h1>Task Manager</h1>
+
+        <form
+          onSubmit={handleAddTask}
+          className="input-section"
         >
-          Learn React
-        </a>
-      </header>
+          <input
+            type="text"
+            placeholder="Enter Task"
+            value={taskNameInput}
+            onChange={(e) =>
+              setTaskNameInput(e.target.value)
+            }
+          />
+
+          <button type="submit">
+            Add
+          </button>
+        </form>
+
+        <div className="clear-wrapper">
+          <span onClick={handleClearList}>
+            Clear list
+          </span>
+        </div>
+
+        <div className="task-container">
+
+          {tasks.length === 0 ? (
+            <p className="no-task">
+              No Task
+            </p>
+          ) : (
+            <>
+              <h2>Ongoing Task</h2>
+
+              {tasks.map((task) => (
+                <div
+                  key={task.id}
+                  className="task-item"
+                >
+                  <div className="task-name">
+                    {task.name}
+                  </div>
+
+                  <button
+                    className="checkbox"
+                    onClick={() =>
+                      dispatch(deleteTask(task.id))
+                    }
+                  ></button>
+
+                </div>
+              ))}
+            </>
+          )}
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
-
-export default App;
---
+++ simple-task-manager/src/feature/taskSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+const taskSlice = createSlice({
+  name: "tasks",
+  initialState,
+
+  reducers: {
+    addTask: (state, action) => {
+      state.push(action.payload);
+    },
+
+    deleteTask: (state, action) => {
+      return state.filter(
+        (task) => task.id !== action.payload
+      );
+    },
+  },
+});
+
+export const { addTask, deleteTask } =
+  taskSlice.actions;
+
+export default taskSlice.reducer;
